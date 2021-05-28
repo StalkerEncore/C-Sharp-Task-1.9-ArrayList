555cd94cea6a1910c7a2840f4876ba1648e670d1
StalkerEncore <fart.stalker@yandex.ru>
2021-05-28 17:28:21 +0300

    VSE GOTOVO, NASRAL DELEGATOM

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,13 @@ namespace C_Sharp_Task_1._9_ArrayList
 {
     public partial class Form1 : Form
     {
-       
         TicketOffice tickets = new TicketOffice("text.txt");
-  
+        TicketOffice bought = new TicketOffice();
+        bool tag = false; //Переменная, помогающая разделять реализацию вывода списка билетов в зависимости от того, какая кнопка была нажата перед покупкой
+
+        private delegate void BuyTicket(object sender, EventArgs e);
+        private event BuyTicket OnAdd;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,26 +26,105 @@ namespace C_Sharp_Task_1._9_ArrayList
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("\n лол");
-            tickets.AddTickets(new Ticket(100, "19.02.2000", 10, true, "Хлеб"));
-            tickets.AddTickets(new Ticket(100, "15.02.2002", 10, true, "Хле2б"));
-            //tickets.GetFreeSeatsForDate("10.02.2000", "5.03.2000");
-            ShowTickets(sender, e); 
-            tickets.GetFreeSeatsForCost(100, 200);
+            comboBox1.Items.Add("Показать все");
+            comboBox2.Items.Add("Показать все");
+            OnAdd += BuyingTickets;
+            foreach (Ticket ticket in tickets.GetTickets())
+            {
+                if (!comboBox1.Items.Contains(ticket.Name))
+                {
+                    comboBox1.Items.Add(ticket.Name);
+                }
 
+                if (!comboBox2.Items.Contains(ticket.Place))
+                {
+                    comboBox2.Items.Add(ticket.Place);
+                }
+            }
+        }
+        private void ShowTickets(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
             foreach (Ticket ticket in tickets.GetSortedTicket())
             {
-                Console.WriteLine(ticket.Cost.ToString() + " " + ticket.Date + " " + ticket.Name);
+                if (ticket.Vacant == "свободно")
+                {
+                    listBox1.Items.Add(ticket.Numb + " \" " + ticket.Name + "\" " + ". " + ticket.Cost + "руб. " + ticket.Date + " " + ticket.Row + "-ый ряд " + ticket.Seat + "-ое место, " + ticket.Place + ", сейчас " + ticket.Vacant);
+                }
             }
         }
 
-        private void ShowTickets(object sender, EventArgs e)
+
+        private void button2_Click(object sender, EventArgs e)
         {
-            foreach (var ticket in tickets.GetTickets())
+            tag = true;
+            tickets.GetFreeSeatsForDate(textBox2.Text.ToString(), textBox3.Text.ToString(), comboBox1.Text, comboBox2.Text);
+            ShowTickets(sender, e);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tag = true;
+            tickets.GetFreeSeatsForCost(Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text), comboBox1.Text, comboBox2.Text);
+            ShowTickets(sender, e);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tag = true;
+            tickets.GetFreeSeatsForTickets(Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox7.Text), Convert.ToInt32(textBox8.Text), Convert.ToInt32(textBox9.Text), comboBox1.Text, comboBox2.Text);
+            ShowTickets(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tag = false;
+            listBox1.Items.Clear();
+            foreach (Ticket ticket in tickets.GetTickets())
             {
-                textBox1.Text += ticket.Name + " " + ticket.Cost + " " + ticket.Date + Environment.NewLine;
+                listBox1.Items.Add(ticket.Numb + " \" " + ticket.Name + "\" " + ". " + ticket.Cost + "руб. " + ticket.Date + " " + ticket.Row + "-ый ряд " + ticket.Seat + "-ое место, " + ticket.Place + ", сейчас " + ticket.Vacant);
             }
-            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Location = this.Location;
+            form.StartPosition = FormStartPosition.Manual;
+            form.FormClosing += delegate { this.Show(); };
+            form.Show();
+            this.Hide();
+
+            OnAdd?.Invoke(sender, e);
+
+            foreach (Ticket ticket in bought.GetTickets())
+            {
+                form.listBox1.Items.Add(ticket.Numb + " \" " + ticket.Name + "\" " + ". " + ticket.Date + " " + ticket.Row + "-ый ряд " + ticket.Seat + "-ое место, " + ticket.Place);
+            }
+
+            if (tag == true) ShowTickets(sender, e);
+            else
+            {
+                listBox1.Items.Clear();
+                foreach (Ticket ticket in tickets.GetTickets()) listBox1.Items.Add(ticket.Numb + " \" " + ticket.Name + "\" " + ". " + ticket.Cost + "руб. " + ticket.Date + " " + ticket.Row + "-ый ряд " + ticket.Seat + "-ое место, " + ticket.Place + ", сейчас " + ticket.Vacant);
+            }
+        }
+
+        private void BuyingTickets(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string str = listBox1.SelectedItem.ToString();
+                string[] numb = str.Split('\"');
+                foreach (Ticket ticket in tickets.GetTickets())
+                {
+                    if (ticket.Numb == Convert.ToInt32(numb[0]))
+                    {
+                        ticket.Vacant = "ПРОДАНО";
+                        bought.AddTicket(ticket);
+                    }
+                }
+            }
         }
     }
 }
--- Ticket.cs
+++ Ticket.cs
@@ -3,21 +3,32 @@
     class Ticket
     {
      
-        public Ticket(int cost, string Date, int Seat, bool Vacant, string Name)
+        public Ticket(int Numb, int cost, string Date, int Row, int Seat, string Place, string Vacant, string Name)
         {
+            this.Numb = Numb;
             this.Date = Date; 
             this.Cost = cost;
-            this.Site = Seat;
+            this.Row = Row;
+            this.Seat = Seat;
+            this.Place = Place;
             this.Vacant = Vacant;
             this.Name = Name;
-        }
-        public Ticket() { } 
 
+        }
+        public Ticket() { }
+
+        public int Numb { set; get; }
         public string Date { set; get; }
         public string Name { set; get; }
-        public int Site { set; get; }
+        public int Seat { set; get; }
+        public int Row { set; get; }
+        public string Place { set; get; }
         public int Cost { set; get; }
-        public bool Vacant { set; get; }
-  
+        public string Vacant { set; get; }
+
+        public string Info() 
+        {
+            return Numb.ToString() + Cost.ToString() + Date + Row.ToString() + Seat.ToString() + Place + Vacant + Name; 
+        }
     }
 }
